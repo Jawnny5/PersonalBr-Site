8d261db95c5a930d6c61b1c78c3929582f9b73b9
“Jawnny5” <digitalanalogbros@gmail.com>
2021-05-14 10:39:38 -0600

    icons and links to socials

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backdropFilter: {
+      'none': 'none',
+      'blur': 'blur(20px)'
+    },
+    colors: {
+      'brown': 'hsl(46, 19%, 30%)',
+      'green': 'hsl(144, 25%, 34%)',
+      'blue': 'hsl(190, 86%, 88%)',
+      'gray': 'hsl(0, 0%, 76%)'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
